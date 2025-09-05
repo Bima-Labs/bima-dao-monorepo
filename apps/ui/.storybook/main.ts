65d983768eb43ef356f48b1054ad36592b548ac5
devdatta <devdattanarawade97@gmail.com>
2025-09-05 22:48:47 +0530

    fix(ui): Suppress Semgrep false positive for path traversal in Storybook config

--- apps/ui/.storybook/main.ts
+++ apps/ui/.storybook/main.ts
@@ -7,7 +7,7 @@ import { join, dirname } from 'path';
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
+  return dirname(require.resolve(join(value, 'package.json'))); // semgrep-ignore: javascript.lang.security.audit.path-traversal.path-join-resolve-traversal
 }
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
